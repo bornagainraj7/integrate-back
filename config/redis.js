a4d08b225e9ffddb9e8823806ac91c346941e244
Raj Kumar <rajkumar@insurancesamadhan.com>
2020-05-22 00:51:13 +0530

    lint fixes

--- config/redis.js
+++ config/redis.js
@@ -1,4 +1,5 @@
 const redis = require('redis');
+const logger = require('tracer').colorConsole();
 
 // setup port constants
 const portRedis = process.env.PORT || 6379;
@@ -6,8 +7,8 @@ const portRedis = process.env.PORT || 6379;
 // configure redis client on port 6379
 const redisClient = redis.createClient(portRedis);
 
-redisClient.on('connect', (err, response) => {
-  console.log('Connected to redis database');
+redisClient.on('connect', () => {
+  logger.info('Connected to redis database');
 });
 
 module.exports = redisClient;
